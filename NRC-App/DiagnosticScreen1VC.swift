f57bcbdf66b040a30ea08fdada4e415c87492563
Sachin Sulkunte <sachinsulkunte@sachins-macbook-air.local>
2018-07-29 23:33:48 -0400

    updated with diagnostics

---
+++ NRC-App/DiagnosticScreen1VC.swift
@@ -0,0 +1,27 @@
+//
+//  DiagnosticScreen1VC.swift
+//  NRC-App
+//
+//  Created by Sachin Sulkunte on 7/29/18.
+//  Copyright © 2018 NRC-interns. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosticScreen1VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
