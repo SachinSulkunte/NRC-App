3ff030d4181ed6adf17a8e623664f87110f9ad0d
angela sun <angelasun@macbook-air.local>
2018-08-03 16:45:31 -0400

    added coding file

---
+++ NRC-App/IntroVCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IntroVCViewController.swift
+//  NRC-App
+//
+//  Created by angela sun on 8/3/18.
+//  Copyright © 2018 NRC-interns. All rights reserved.
+//
+
+import UIKit
+
+class IntroVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
