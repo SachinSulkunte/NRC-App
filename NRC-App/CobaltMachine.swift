b88da7e12751e5c377a8b31efd713304bb2c85ba
angela sun <angelasun@macbook-air.local>
2018-08-19 21:40:29 -0400

    Added some pictures, need to close to refresh

---
+++ NRC-App/CobaltMachine.swift
@@ -0,0 +1,37 @@
+//
+//  CobaltMachine.swift
+//  NRC-App
+//
+//  Created by angela sun on 8/19/18.
+//  Copyright © 2018 NRC-interns. All rights reserved.
+//
+
+import UIKit
+
+class CobaltMachine: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SwitchPicture(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
